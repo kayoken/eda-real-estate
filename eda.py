53a6a903ff2b8a2fbf3deae779b063e8f038ccce
kayoken <kayoken@users.noreply.github.com>
2024-12-18 12:07:47 +0100

    exploring data set

--- eda.py
+++ eda.py
@@ -1,5 +1,23 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import warnings
 import pandas as pd
+from matplotlib.ticker import PercentFormatter
 
-df_houses = pd.read_csv(
-    './data/eda.csv')
-print(df_houses)
+warnings.filterwarnings("ignore")
+
+
+def init():
+    plt.rcParams.update({"figure.figsize": (
+        8, 5), "axes.facecolor": "white", "axes.edgecolor":  "black"})
+    plt.rcParams["figure.facecolor"] = "w"
+    pd.plotting.register_matplotlib_converters()
+    pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+    df_houses = pd.read_csv('./data/eda.csv')
+    print(df_houses)
+
+
+if __name__ == '__main__':
+    init()
